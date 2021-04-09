aedd301197e9171f2b0fba4626635eeac058b7f1
gianlucaroccasalvo <gianlucaroccasalvo@gmail.com>
2021-04-09 11:58:24 +0200

    rename core and test functions

--- test_price_function.py
+++ test_price_function.py
@@ -1,6 +1,6 @@
 # DOCUMENTATION: this script contains the test function that will check the validity of price() function.
 
-from core_function import price
+from price_function import price
 
 def test_price():
     assert price(5, 0.3, 4, 0.7) == 4.3
